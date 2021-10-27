29b0d6ca5e2d9c6739d70d6d3e5fbcc6d8ffd04b
jbrasko <jbrasko0508@gmail.com>
2021-10-27 11:13:46 -0500

    create new div and insert into dom

--- dom.js
+++ dom.js
@@ -17,7 +17,6 @@
 // }
 
 // querySelector
-
 // const header = document.querySelector('#main-header');
 // header.style.borderBottom = '4px solid #ccc';
 
@@ -37,16 +36,31 @@
 // secondItem.style.color = 'coral';
 
 // querySelectorAll
+// const titles = document.querySelectorAll('.title');
 
-const titles = document.querySelectorAll('.title');
+// console.log(titles);
+// titles[0].textContent = 'Howdy';
 
-console.log(titles);
-titles[0].textContent = 'Howdy';
+// const odd = document.querySelectorAll('li:nth-child(odd)');
+// const even = document.querySelectorAll('li:nth-child(even)');
 
-const odd = document.querySelectorAll('li:nth-child(odd)');
-const even = document.querySelectorAll('li:nth-child(even)');
+// for (let i = 0; i < odd.length; i++) {
+//   odd[i].style.backgroundColor = '#f4f4f4';
+//   even[i].style.backgroundColor = '#ccc';
+// }
 
-for (let i = 0; i < odd.length; i++) {
-  odd[i].style.backgroundColor = '#f4f4f4';
-  even[i].style.backgroundColor = '#ccc';
-}
+// Create div, insert into DOM
+const newDiv = document.createElement('div');
+
+newDiv.className = 'hello';
+
+newDiv.setAttribute('title', 'Hello Div');
+
+const newDivText = document.createTextNode('Hello World');
+
+newDiv.appendChild(newDivText);
+
+const container = document.querySelector('header #container');
+const h1 = document.querySelector('header h1');
+
+container.insertBefore(newDiv, h1);
