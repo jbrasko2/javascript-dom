696465e0fbe19eab58982a1b8d6320ad695d737e
jbrasko <jbrasko0508@gmail.com>
2021-10-27 09:54:23 -0500

    create for loop to style htmlcollection

--- dom.js
+++ dom.js
@@ -2,3 +2,14 @@ const headerTitle = document.getElementById('header-title');
 const header = document.getElementById('main-header');
 
 header.style.borderBottom = '3px solid black';
+
+const items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[1].textContent = 'Hello';
+items[1].style.fontWeight = 'bold';
+items[1].style.backgroundColor = 'yellow';
+
+for (let i = 0; i < items.length; i++) {
+  items[i].style.backgroundColor = '#f4f4f4';
+}
